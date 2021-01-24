b630fce97696224812bd3be9c1f068652b0563ab
gideon-esl <gfernandes@eslsistemas.com.br>
2021-01-24 17:19:03 -0300

    @web/add global styles

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const App: React.FC = () => {
-  return <h1>Hello World from GoBarber Web!</h1>;
-};
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <h1>Hello World from GoBarber Web!</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
